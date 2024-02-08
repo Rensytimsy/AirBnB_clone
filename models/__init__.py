db86b3efa9183b8f0af4fb92bf3149c9087c4379
samkamau254 <samkamau254@outlook.com>
2024-02-08 05:16:02 -0500

    test

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This module creates an instance of FileStorage
-It imports the models engine filestorage
-"""
-
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
