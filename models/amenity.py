af52d3b0b51a37bff38104005776ac1942825980
Rensytimsy <rensytimsy03@gmail.com>
2024-02-08 10:40:12 +0300

    some changes updated on the previous files

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""This module contains the Amenity class
+"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
-
-    Attributes:
-        name (str): The name of the amenity.
+    """The Amenity class contains the name attribute
     """
-
     name = ""
--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,65 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""This module contains the BaseModel class that
+defines all common attributes/methods for other classes
+"""
+import uuid
 from datetime import datetime
+from datetime import date
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """This BaseModel class contains the methods and
+    attributes for other classes
+    """
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+        """This is used to initialize the attributes
+    and set up the initial state of the object
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            from models import storage
+            storage.new(self)
         else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == "updated_at":
+                    value = datetime.strptime(value, format)
+                setattr(self, key, value)
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """returns the informal string representation of the object
+
+        Returns:
+            str: informal description
+        """
+        return ("[{}] ({}) {}".
+                format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at with
+        the current datetime
+        """
+        self.updated_at = datetime.now()
+        from models import storage
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance
+
+        Returns:
+            dict: instance dictionary
+        """
+        dict_copy = self.__dict__.copy()
+        if 'created_at' in dict_copy:
+            dict_copy['created_at'] = dict_copy['created_at'].isoformat()
+        if 'updated_at' in dict_copy:
+            dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
+
+        dict_copy['__class__'] = self.__class__.__name__
+        return dict_copy
